eb18d100b1a2d6d322291a963c7af0ca4ab206eb
kuspen <lilimarleen.physalis2@gmail.com>
2020-01-06 01:02:20 +0900

    add server send, recv private method and camera class.

--- serverapp/server.py
+++ serverapp/server.py
@@ -2,7 +2,7 @@ import socket.ReptilesSocket
 
 # header 2[byte], length 4[byte], data length[byte]
 
-HEADER_GET_IMG_DATA = 0x1000
+__HEADER_GET_IMG_DATA = 0x1000
 
 class ReptilesServer:
 
@@ -12,11 +12,31 @@ class ReptilesServer:
     def connect(self):
         self.socket.connect()
 
-    def run(self):
-        pass
+    def get_img_data(self):
+        header = __recv_header()
+        length = __recv_length()
+        data = __recv_data(length)
 
-#    def getImgData(self):
-#        self.socket.send(HEADER_GET_IMG_DATA.to_bytes)
+        __send_header(__HEADER_GET_IMG_DATA)
+        __send_length()
 
+    def __send_header(header):
+        send_data = header.to_bytes(2, 'little')
+        self.socket(send_data)
 
+    def __send_length(length):
+        send_data = header.to_bytes(4, 'little')
+        self.socket(send_data)
+
+    def __recv_header():
+        header = self.socket(2)
+        return int.from_bytes(header, 'little')
+
+    def __recv_length():
+        length = self.socket(4)
+        return int.from_bytes(length, 'little')
+
+    def __recv_data(length):
+        data = self.socket(length)
+        return int.from_bytes(data, 'little')
 
